486929cac635ed421985184ef2685e2dd0217f13
ICMIX13 <53002202+ICMIX13@users.noreply.github.com>
2019-07-17 18:44:48 +0700

    Update DateRangeIterator.java

--- DateRangeIterator.java
+++ DateRangeIterator.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testdaterange;
 
 import java.time.LocalDate;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- *
- * @author Asus
- */
 public class DateRangeIterator implements Iterator<LocalDate> {
     LocalDate awal,akhir,sekarang;
     
@@ -26,7 +17,6 @@ public class DateRangeIterator implements Iterator<LocalDate> {
         return sekarang.plusDays(1).isBefore(akhir);
     }
 
-    @Override
     public LocalDate next() {
         if (!hasNext()) throw new NoSuchElementException();
         LocalDate now = this.sekarang;
